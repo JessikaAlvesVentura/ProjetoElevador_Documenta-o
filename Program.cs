f4a4eabd446430af08ee8dc2cf6ea277534c4c83
JessikaAlvesVentura <jehalves34@gmail.com>
2022-06-23 14:35:27 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador_Documentação
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+        //Elevador: -teremos 6 andares + térreo Primeira visualização do usuário: 
+        //-Olá, bem vindo aoSoftware de elevador. Nossa capacidade máxima é de 5 pessoas.
+        //Por gentileza, escolha o andar desejado: 
+        //Interação informada pelo usuário: -Entrar no elevador 
+        //-Ou aguardar o próximo Se escolher entrar no elevador: 
+        //-Informar quantas pessoas entrarão - Se o elevador estiver atingido a capacidade 
+        //máxima, informará para o usuário aguardar o próximo por excesso. 
+        //-Se não tiver ninguém e/ou tiver menos que 4 pessoas, pedir para o usuário 
+        //entrar. 
+        //-E informar o andar desejado
+        //Se escolher aguardar o próximo, o programa informará que ele permanece no andar 
+        //e pede para escolher novamente entre entrar ou aguardar.
+        //Movimento do elevador: Iniciará no Térreo e vazio. O usuário informará se quer 
+        //entrar ou aguardar o próximo. 
+        //Se entrar, informar quantas pessoas entrarão e o andar desejado.Subirá.
+        //Quando chegar no andar, escolherá entre sair do elevador ou continuar no elevador.
+        //Se sair, informará que saiu e voltará a informar se quer entrar ou aguardar.
+        //Se continuar, pede novamente o andar desejado. Subirá ou descerá.
+        
+        }
+
+        
+    }
+}
